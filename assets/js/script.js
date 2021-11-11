f83c888da652b660bda1a6140a1d81f1b950855e
meenaalemijalal <meena.maj58@gmail.com>
2021-11-10 20:45:59 -0800

    add items to bring input field and listing item done

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ var cityForm = document.querySelector('#cityForm')
 var cityName = document.querySelector('#city')
 var submit = document.querySelector('#submit')
 var modal = document.querySelector('#mainModal')
+const listItemsContainer= document.getElementById("list-items")
+const itemInput= document.getElementById("item-input")
+const itemsNull = document.getElementById('items-null')
 
 // grabs input to verify if city was input, then calls another function (Would be API to grab lat/long)
 var citySearch = function (event) {
@@ -15,3 +18,15 @@ var citySearch = function (event) {
 
 // Event Listener
 submit.addEventListener("click", citySearch)
+
+function addItemsToBring(event) {
+    if(event.keyCode===13 && itemInput.value){
+       const itemName= itemInput.value
+       const listItem=document.createElement('li')
+       listItem.innerHTML = itemName
+       itemsNull.style.display = 'none'
+       listItemsContainer.appendChild(listItem)
+       itemInput.value=''
+    }
+}
+itemInput.addEventListener('keyup', addItemsToBring)
