fef954f4a78d580429df01ba21ac2a79b843581c
Robert Sandoval <88403035+Varnen92@users.noreply.github.com>
2021-11-15 21:03:45 -0800

    Revert "api add"

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,48 +16,9 @@ var citySearch = function (event) {
     city = cityName.value.trim()
     if (city) {
         // function would go here to be called
-        town(city)
         cityName.value = ""
         modal.classList.remove("is-active");
     }
-
-}
-
-var town = function (city) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=6d1d7721cce65133dca83415077d7208&units=imperial"
-    fetch(apiUrl)
-        .then(function (response) {
-
-            if (response.ok) {
-                console.log(apiUrl)
-                //cityHistory.push(city)
-
-                response.json().then(function (data) {
-                    console.log(data)
-                    var flat = data.coord.lat
-                    var vert = data.coord.lon
-                    // pollen api
-                    var pollen = "https://api.breezometer.com/air-quality/v2/current-conditions?lat=" + flat + "&lon=" + vert + "&key=453aa725e6e640bea18ebffa1017eba3"
-                    fetch(pollen)
-                        .then(poly1 => poly1.json())
-                        .then(poly2 => { console.log(poly2)})
-
-                    console.log(flat, vert)
-                })
-            } else {
-                alert("Not a valid city name!")
-            }
-        })
-};
-
-
-
-// Event Listener
-submit.addEventListener("click", citySearch)
-
-
-
-
     saveCity(city);
 }
 
@@ -215,4 +176,3 @@ const listItemsInStorage = JSON.parse(localStorage.getItem('itemsList'))
 if(listItemsInStorage.length) {
   renderItemsToDOM()
 }
-
